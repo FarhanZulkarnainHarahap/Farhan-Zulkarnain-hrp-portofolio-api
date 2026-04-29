77d0fb4e74c1d94335384329897a5b82e21ce2c3
NexxusFz <farhanzulkarnaenhrp@gmail.com>
2026-04-29 21:53:37 +0700

    second commit

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,8 @@ const PORT = process.env.PORT || 8000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: 'http://localhost:3000',
+  methods: ["GET", "POST", "PUT", "DELETE"], // Atur metode HTTP yang diizinkan
   credentials: true,
 }));
 
